ab43aad0d7a59c7fbc11e1988af94e9026eeb343
Hannah Karlsson <gamemod.mod@gmail.com>
2022-08-29 10:05:31 +0200

    Ny formatering :)

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Xml;
+﻿//Fråga användaren: "Vad är ditt namn?"
+Console.WriteLine("Vad är ditt namn?");
 
-namespace MyFirstApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //Fråga användaren: "Vad är ditt namn?"
-            Console.WriteLine("Vad är ditt namn?");
+//Hämta namnet från vad användaren skriver!
+string? name = Console.ReadLine(); //string? då Console.ReadLine() krävde det.
 
-            //Hämta namnet från vad användaren skriver!
-            string? name = Console.ReadLine(); //string? då Console.ReadLine() krävde det.
-
-            //Skriv ut "Hej! Ditt namn är (Vad användaren skrev in)"
-            Console.WriteLine($"Hej! Ditt namn är {name}");
-        }
-    }
-}
+//Skriv ut "Hej! Ditt namn är (Vad användaren skrev in)"
+Console.WriteLine($"Hej! Ditt namn är {name}");
