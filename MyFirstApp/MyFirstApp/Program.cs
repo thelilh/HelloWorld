7356fac91f7d47035b54b0d2ea797b01f95cd511
Hannah Karlsson <gamemod.mod@gmail.com>
2022-08-29 14:47:19 +0200

    La till frukost och ålder

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -2,7 +2,11 @@
 Console.WriteLine("Vad är ditt namn?");
 
 //Hämta namnet från vad användaren skriver!
-string? name = Console.ReadLine(); //string? då Console.ReadLine() krävde det.
+var name = Console.ReadLine();
+var frukost = true;
+var age = 20;
 
 //Skriv ut "Hej! Ditt namn är (Vad användaren skrev in)"
-Console.WriteLine($"Hej! Ditt namn är {name}");
+Console.WriteLine($"Hej! Ditt namn är {name}, du är {age} år och..");
+if(frukost) { Console.WriteLine("..du hade frukost!"); }
+else { Console.WriteLine("..du hade inte frukost!"); }
