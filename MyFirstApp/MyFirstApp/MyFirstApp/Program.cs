3eac88ee18afd1cfce9eea524ac45ba652dae9fc
Hannah Karlsson <gamemod.mod@gmail.com>
2022-08-29 09:26:54 +0200

    Introduktionsövning för den 2022-08-31

--- MyFirstApp/MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/MyFirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vad är ditt namn?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hej! Ditt namn är {name}");
         }
     }
 }
