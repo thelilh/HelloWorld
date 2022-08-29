699099817cdfeb6aac584b2d89d27c79d4a6ef3c
Hannah Karlsson <gamemod.mod@gmail.com>
2022-08-29 09:37:51 +0200

    Kopierade fel.

--- MyFirstApp/MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/MyFirstApp/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Xml;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyFirstApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Fråga användaren: "Vad är ditt namn?"
             Console.WriteLine("Vad är ditt namn?");
-            string? name = Console.ReadLine();
+
+            //Hämta namnet från vad användaren skriver!
+            string? name = Console.ReadLine(); //string? då Console.ReadLine() krävde det.
+
+            //Skriv ut "Hej! Ditt namn är (Vad användaren skrev in)"
             Console.WriteLine($"Hej! Ditt namn är {name}");
         }
     }
